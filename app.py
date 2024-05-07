5a4dfada29f517f60bc206ae9a1c29da321bb41d
Isabella <isabellagalvaosantos9@gmail.com>
2024-05-07 12:14:38 -0300

    rota index e main

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 from api import bp
 app.register_blueprint(bp)
  
- if __name__ == "__main__" :
+if __name__ == "__main__" :
     app.run(host="0.0.0.0")
--- distancia.py
+++ distancia.py
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 from ast import main
+from os import name
 
 
-int main() {
-    char name[50]; // Definindo uma string para armazenar o nome do motorista
+int main() 
+{
+    chr name[50]; // Definindo uma string para armazenar o nome do motorista
     float distancia, velocidade, tempo;
 
     printf("Digite o nome do motorista: ");
