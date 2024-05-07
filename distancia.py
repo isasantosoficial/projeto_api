a69d287164146130c75be03bcc735ae3c0a67bac
Isabella <isabellagalvaosantos9@gmail.com>
2024-05-07 08:24:15 -0300

    criando pastas para templates

---
+++ distancia.py
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+from ast import main
+
+
+int main() {
+    char name[50]; // Definindo uma string para armazenar o nome do motorista
+    float distancia, velocidade, tempo;
+
+    printf("Digite o nome do motorista: ");
+    scanf("%s", nome); // Lendo o nome do motorista
+
+    printf("Digite a distância a percorrer em km: ");
+    scanf("%f", &distancia);
+
+    printf("Digite a velocidade média em km/h: ");
+    scanf("%f", &velocidade);
+
+    tempo = distancia / velocidade;
+
+    printf("O motorista %s levará %.2f horas para fazer o percurso.\n", nome, tempo);
+
+    return 0;
+}
